1beb20b1f0ac393b2207cfaeeda41bbd3ed6e2e6
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2020-10-20 21:50:57 +0100

    Added background component
    Sloted background component with image as prop

---
+++ components/background.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="background--container">
+		<div class="overlay">
+			<slot></slot>
+		</div>
+		<img :src="image" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		image: {
+			type: String,
+			default: '/Image.png'
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/theming.scss';
+
+#background--container {
+	height: 100vh;
+	width: 100vw;
+	position: relative;
+	background: $bg;
+	overflow: hidden;
+
+	img {
+		position: absolute;
+		top: 10%;
+		left: 40px;
+		mix-blend-mode: multiply;
+		opacity: 0.8;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,32 @@
 <template>
-	<div class="container">
-		<div class="flex flex-col items-center">
-			<img src="~/assets/images/gfinity-logo.png" alt="gfinity-logo" />
-			<dummy-component></dummy-component>
-			<div class="links">
-				<a
-					href="https://nuxtjs.org/"
-					target="_blank"
-					class="button--green"
-				>
-					Documentation
-				</a>
-				<a
-					href="https://github.com/nuxt/nuxt.js"
-					target="_blank"
-					class="button--grey"
-				>
-					GitHub
-				</a>
-			</div>
+	<background>
+		<div>
+			Hello amte
 		</div>
-	</div>
+		<div>
+			Hello amte
+		</div>
+		<div>
+			Hello amte
+		</div>
+		<div>
+			Hello amte
+		</div>
+		<div>
+			Hello amte
+		</div>
+	</background>
 </template>
-<script>
-import DummyComponent from '~/components/dummy-component'
 
+<script>
+import Background from '@/components/background'
 export default {
 	components: {
-		DummyComponent
+		Background
 	}
 }
 </script>
-<style>
-.container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
 
-.button--green {
-	display: inline-block;
-	border-radius: 4px;
-	border: 1px solid #3b8070;
-	color: #3b8070;
-	text-decoration: none;
-	padding: 10px 30px;
-}
-
-.button--green:hover {
-	color: #fff;
-	background-color: #3b8070;
-}
-
-.button--grey {
-	display: inline-block;
-	border-radius: 4px;
-	border: 1px solid #35495e;
-	color: #35495e;
-	text-decoration: none;
-	padding: 10px 30px;
-	margin-left: 15px;
-}
-
-.button--grey:hover {
-	color: #fff;
-	background-color: #35495e;
-}
+<style lang="scss" scoped>
+@import '@/assets/scss/theming.scss';
 </style>
