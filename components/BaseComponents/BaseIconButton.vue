fb6582eead9c6cbbfd59cae75d4705bf35510587
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2020-10-21 02:05:00 +0100

    Added icons and base icon button

---
+++ components/BaseComponents/BaseIconButton.vue
@@ -0,0 +1,18 @@
+<template>
+	<button class="w-auto h-auto" @click="$emit('click')">
+		<img :src="icon" />
+	</button>
+</template>
+
+<script>
+export default {
+	props: {
+		icon: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
